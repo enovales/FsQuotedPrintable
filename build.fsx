b9f38cf5c5017170c726748ab77947660d299815
Erik Novales <enovales@codehitmen.com>
2021-05-24 01:06:28 -0700

    fix username

--- build.fsx
+++ build.fsx
@@ -71,7 +71,7 @@ let docsDir = __SOURCE_DIRECTORY__  @@ "docs"
 let docsSrcDir = __SOURCE_DIRECTORY__  @@ "docsSrc"
 let docsToolDir = __SOURCE_DIRECTORY__ @@ "docsTool"
 
-let gitOwner = "MyGithubUsername"
+let gitOwner = "enovales"
 let gitRepoName = "FsQuotedPrintable"
 
 let gitHubRepoUrl = sprintf "https://github.com/%s/%s" gitOwner gitRepoName
